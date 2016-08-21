bede670ca4656c36fc0ccf95d7ce5baecf29c63c
joncravotta <joncravotta32@gmail.com>
2016-08-21 17:56:50 -0400

    init app with comapss and webpack setup

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from './Navigation.jsx'
+
+class App extends React.Component {
+   render() {
+      return (
+         <Navigation/>
+      );
+   }
+}
+
+export default App;
---
+++ Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+   render() {
+      return (
+         <div className="navbar">
+            Nighty Oats
+         </div>
+      );
+   }
+}
+
+export default Navigation;
