bb03c1b34fae8f6a3bcac36d71ae798e56212482
joncravotta <joncravotta32@gmail.com>
2016-08-22 20:31:05 -0400

    more ui stuff

--- Home.jsx
+++ Home.jsx
@@ -4,7 +4,11 @@ class Home extends React.Component {
    render() {
       return (
          <div>
+            <div className="home-order-nav">
+            <div className="home-start-order-button">ORDER NOW</div>
+            </div>
             <div className="home-landing">
+              <div className="nav-logo"><img src={'./assets/logos/nighty-oats-green-logo.png'} /></div>
             </div>
             <div className="home-cta"></div>
          </div>
